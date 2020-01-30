a79d1489345903cd950dcef1bbb0708340aeec95
kawasima <kawasima1016@gmail.com>
2020-01-30 14:37:51 +0900

    Add package.json

--- client.js
+++ client.js
@@ -1,16 +1,16 @@
 /*
- * Copyright (c) 2012 Yoshitaka Kawashima
- * 
+ * Copyright (c) 2012-2020 Yoshitaka Kawashima
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,50 +21,51 @@
  *
  */
 
-var config = require('config').server;
-var client = require('socket.io-client');
-var http = require('http');
+const client = require('socket.io-client');
+const http = require('http');
+const host = process.env.TRANSMITTER_HOST || 'localhost';
+const port = process.env.TRANSMITTER_PORT || 8081;
 
 client.transports = ['xhr-polling'];
 
-var socket = client.connect('http://' + config.host + ':' + config.transmitter.port);
+const socket = client.connect('http://' + host + ':' + port);
 
 socket.on('delegate request', function (options) {
-    delete options.headers["accept-encoding"];
-    var postdata = options.postdata;
-    delete options.postdata;
+  delete options.headers["accept-encoding"];
+  const postdata = options.postdata;
+  delete options.postdata;
 
-    var request = http.request(
-	options,
-	function (response) {
-	    socket.emit('response header', {
-		requestId: options["requestId"],
-		statusCode: response.statusCode,
-		headers: response.headers
-	    });
+  const request = http.request(
+    options,
+    function (response) {
+      socket.emit('response header', {
+	requestId: options["requestId"],
+	statusCode: response.statusCode,
+	headers: response.headers
+      });
 
-	    response.on('data', function(chunk){
-		socket.emit('response data', {
-		    requestId: options["requestId"],
-		    data: chunk.toString('base64')
-		});
-	    });
-
-	    response.on('end', function(){
-		socket.emit('response end', {
-		    requestId: options["requestId"]
-		});
-	    });
-
+      response.on('data', function(chunk){
+	socket.emit('response data', {
+	  requestId: options["requestId"],
+	  data: chunk.toString('base64')
 	});
+      });
 
-    request.on('error', function (error) {
-	console.log('Error:' + error);
+      response.on('end', function(){
+	socket.emit('response end', {
+	  requestId: options["requestId"]
+	});
+      });
+
     });
 
-    if (postdata)
-	request.write(postdata);
+  request.on('error', function (error) {
+    console.log('Error:' + error);
+  });
 
-    request.end();
+  if (postdata) {
+    request.write(postdata);
+  }
 
+  request.end();
 });
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 /*
- * Copyright (c) 2012 Yoshitaka Kawashima
- * 
+ * Copyright (c) 2012-2020 Yoshitaka Kawashima
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,86 +20,85 @@
  * THE SOFTWARE.
  *
  */
+const url  = require('url');
+const http = require('http');
+const crypto = require('crypto');
 
-var config = require('config').server;
-var url  = require('url');
-var http = require('http');
-var crypto = require('crypto');
+const transmitterPort = process.env.TRANSMITTER_PORT || 8081
+const io = require('socket.io').listen(transmitterPort);
+const responses = {};
 
-var io   = require('socket.io').listen(config.transmitter.port);
-io.set('log level', 1);
+const wsServer = io.sockets.on('connection', function (socket) {
+  socket.on('response header', function (data) {
+    const response = responses[data.requestId];
+    response.writeHead(data.statusCode, data.headers);
+  });
 
-var responses = {};
+  socket.on('response data', function (data) {
+    const buf = new Buffer(data.data, 'base64');
+    const response = responses[data.requestId];
+    response.write(buf);
+  });
 
-var wsServer = io.sockets.on('connection', function (socket) {
-    socket.on('response header', function (data) {
-	var response = responses[data.requestId];
-	response.writeHead(data.statusCode, data.headers);
-    });
-
-    socket.on('response data', function (data) {
-	var buf = new Buffer(data.data, 'base64');
-	var response = responses[data.requestId];
-	response.write(buf);
-    });
-
-    socket.on('response end', function (data) {
-	var response = responses[data.requestId];
-	response.end();
-	delete responses[data.requestId];
-    });
+  socket.on('response end', function (data) {
+    const response = responses[data.requestId];
+    response.end();
+    delete responses[data.requestId];
+  });
 });
 
-var delegateRequest = function (options, res) {
-    for (var i in wsServer.sockets) {
-	var socket = wsServer.sockets[i];
-	
-	var requestId = crypto.randomBytes(64).toString('hex');
-	responses[requestId] = res;
-	options["requestId"] = requestId;
-	socket.emit('delegate request', options);
-    }
+const delegateRequest = function (options, res) {
+  for (let i in wsServer.sockets) {
+    const socket = wsServer.sockets[i];
+
+    const requestId = crypto.randomBytes(64).toString('hex');
+    responses[requestId] = res;
+    options["requestId"] = requestId;
+    socket.emit('delegate request', options);
+  }
 };
 
 
-var proxyServer = http.createServer(function (req, res) {
-    var headers = req.headers;
-    if('proxy-connection' in headers){
-	headers['connection'] = 'close';
-	delete headers['proxy-connection'];
-    }
-    if('cache-control' in headers){
-	delete headers['cache-control'];
-    }
+const proxyServer = http.createServer(function (req, res) {
+  var headers = req.headers;
+  if('proxy-connection' in headers){
+    headers['connection'] = 'close';
+    delete headers['proxy-connection'];
+  }
+  if('cache-control' in headers){
+    delete headers['cache-control'];
+  }
 
-    var targetUrl = url.parse(req.url, false);
+  const targetUrl = url.parse(req.url, false);
 
-    if(!targetUrl.pathname){ targetUrl.pathname = '/'; }
-    if(!targetUrl.search){ targetUrl.search = ''; }
-    if(!targetUrl.port){ targetUrl.port = 80; }
-    var options = {
-	host: targetUrl.hostname,
-	port: targetUrl.port,
-	method: req.method,
-	path: targetUrl.path,
-	headers: req.headers
-    };
+  if (!targetUrl.pathname) { targetUrl.pathname = '/'; }
+  if (!targetUrl.search) { targetUrl.search = ''; }
+  if (!targetUrl.port) { targetUrl.port = 80; }
 
-    if(req.method=='POST') {
-	console.log("POST");
-        var body='';
-        req.on('data', function (data) {
-            body +=data;
-        });
-        req.on('end',function(){
-	    options["postdata"] = body;
-	    delegateRequest(options, res);
-        });
-    }
-    else if(req.method=='GET') {
-	delegateRequest(options, res);
-    }
+  const options = {
+    host: targetUrl.hostname,
+    port: targetUrl.port,
+    method: req.method,
+    path: targetUrl.path,
+    headers: req.headers
+  };
 
+  if (req.method === 'POST') {
+    console.log("POST");
+    let body='';
+    req.on('data', function (data) {
+      body +=data;
+    });
+    req.on('end',function(){
+      options["postdata"] = body;
+      delegateRequest(options, res);
+    });
+  }
+  else if(req.method === 'GET') {
+    delegateRequest(options, res);
+  }
 });
 
-proxyServer.listen(config.proxy.port);
+proxyServer.listen((process.env.PORT || 8080), () => {
+  console.log(`Start proxy`);
+});
